14d17d57275afc5d35056a449aee02b9c0f5ded8
Mazennaji <mazen.naji.1@gmail.com>
2026-08-23 19:58:43 +0300

    feat: add URL path for anomaly view in forecasting module

--- apps/forecasting/forecasting/urls.py
+++ apps/forecasting/forecasting/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import forecast_view
+from .views import forecast_view, anomaly_view
 
 urlpatterns = [
     path('forecast/', forecast_view, name='forecast'),
+    path('anomalies/', anomaly_view, name='anomalies'),
 ]
