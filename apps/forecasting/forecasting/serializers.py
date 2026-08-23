830104757e6b0044f9971c5bdfaa52171b19d20e
Mazennaji <mazen.naji.1@gmail.com>
2026-08-23 14:00:15 +0300

    feat(forecasting): add method field to ForecastRequestSerializer for forecasting options

--- apps/forecasting/forecasting/serializers.py
+++ apps/forecasting/forecasting/serializers.py
@@ -5,3 +5,7 @@ class ForecastRequestSerializer(serializers.Serializer):
     tenant_id = serializers.CharField()
     product_id = serializers.CharField()
     periods_weeks = serializers.IntegerField(default=8, min_value=1, max_value=52)
+    method = serializers.ChoiceField(
+        choices=['prophet', 'moving_average', 'linear_trend'],
+        default='prophet',
+    )
