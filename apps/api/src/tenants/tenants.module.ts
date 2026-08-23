aa1824aa47d80518bd34b50289a43cfd563f8aea
Mazennaji <mazen.naji.1@gmail.com>
2026-08-23 08:52:31 +0300

    fix(api): correct import order in TenantsModule for consistency

--- apps/api/src/tenants/tenants.module.ts
+++ apps/api/src/tenants/tenants.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TenantsController } from './tenants.controller';
 import { TenantsService } from './tenants.service';
+import { TenantsController } from './tenants.controller';
 
 @Module({
   controllers: [TenantsController],
-  providers: [TenantsService]
+  providers: [TenantsService],
 })
 export class TenantsModule {}
