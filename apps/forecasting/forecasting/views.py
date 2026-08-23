719b6bd3136b95b2fd4788a68b7a85b496286b12
Mazennaji <mazen.naji.1@gmail.com>
2026-08-23 13:43:16 +0300

    feat(forecasting): implement forecast_view for demand forecasting requests

--- apps/forecasting/forecasting/views.py
+++ apps/forecasting/forecasting/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ForecastRequestSerializer
+from .services import forecast_demand
 
-# Create your views here.
+
+@api_view(['POST'])
+def forecast_view(request):
+    serializer = ForecastRequestSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    data = serializer.validated_data
+
+    result = forecast_demand(
+        tenant_id=data['tenant_id'],
+        product_id=data['product_id'],
+        periods_weeks=data['periods_weeks'],
+    )
+
+    return Response(result, status=status.HTTP_200_OK)
