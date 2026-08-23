96b8e6109f24a8439553d3f764a9889327bf7261
Mazennaji <mazen.naji.1@gmail.com>
2026-08-23 08:48:42 +0300

    feat(api): add TenantsModule, TenantsController, and TenantsService for tenant management

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { TenantsModule } from './tenants/tenants.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, TenantsModule],
 })
 export class AppModule {}
