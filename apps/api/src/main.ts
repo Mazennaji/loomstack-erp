6f180a1cc9364c2f6b1eeb5a2370f915c27ba223
Mazennaji <mazen.naji.1@gmail.com>
2026-08-23 14:19:11 +0300

    feat: enable CORS for local development

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,6 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
