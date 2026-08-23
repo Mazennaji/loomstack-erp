f011e6184e2adc7306e30fde124920d9e0c7415f
Mazennaji <mazen.naji.1@gmail.com>
2026-08-23 08:58:05 +0300

    feat(api): add authentication and user modules with controllers and services

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { TenantsModule } from './tenants/tenants.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule, TenantsModule],
+  imports: [PrismaModule, TenantsModule, AuthModule, UsersModule],
 })
 export class AppModule {}
