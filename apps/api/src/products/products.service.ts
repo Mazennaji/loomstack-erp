763e8d6979067f25f6a30ce7fe5a66e82c26ced8
Mazennaji <mazen.naji.1@gmail.com>
2026-08-23 09:10:21 +0300

    feat(api): implement ProductsService extending TenantScopedService with Prisma integration

--- apps/api/src/products/products.service.ts
+++ apps/api/src/products/products.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { TenantScopedService } from '../common/tenant-scoped.service';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService extends TenantScopedService<any> {
+  protected model;
+
+  constructor(private prisma: PrismaService) {
+    super();
+    this.model = this.prisma.product;
+  }
+}
