acb3c1dcb2afe5c1c2513d2f0790698c2ec13592
Mazennaji <mazen.naji.1@gmail.com>
2026-08-25 12:43:31 +0300

    feat: add maintenance endpoint to urlpatterns for machine maintenance view

--- apps/forecasting/forecasting/urls.py
+++ apps/forecasting/forecasting/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import forecast_view, anomaly_view
+from .views import forecast_view, anomaly_view, maintenance_view
 
 urlpatterns = [
     path('forecast/', forecast_view, name='forecast'),
     path('anomalies/', anomaly_view, name='anomalies'),
+    path('maintenance/', maintenance_view, name='maintenance'),
 ]
