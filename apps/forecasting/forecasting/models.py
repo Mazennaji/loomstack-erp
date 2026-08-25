bcb4bb9dfc2a8eae8b49b8d59836a975b7a28d75
Mazennaji <mazen.naji.1@gmail.com>
2026-08-25 12:44:20 +0300

    feat: add Machine, MachineUsage, and MaintenanceEvent models with database configurations

--- apps/forecasting/forecasting/models.py
+++ apps/forecasting/forecasting/models.py
@@ -34,3 +34,43 @@ class SalesOrderLine(models.Model):
     class Meta:
         managed = False
         db_table = 'SalesOrderLine'
+        
+
+class Machine(models.Model):
+    id = models.CharField(max_length=255, primary_key=True)
+    name = models.CharField(max_length=255)
+    code = models.CharField(max_length=255)
+    status = models.CharField(max_length=50)
+    maintenance_interval_hours = models.IntegerField(db_column='maintenanceIntervalHours')
+    tenant_id = models.CharField(max_length=255, db_column='tenantId')
+
+    class Meta:
+        managed = False
+        db_table = 'Machine'
+
+
+class MachineUsage(models.Model):
+    id = models.CharField(max_length=255, primary_key=True)
+    date = models.DateTimeField()
+    hours_run = models.FloatField(db_column='hoursRun')
+    cycles = models.IntegerField()
+    machine_id = models.CharField(max_length=255, db_column='machineId')
+    tenant_id = models.CharField(max_length=255, db_column='tenantId')
+
+    class Meta:
+        managed = False
+        db_table = 'MachineUsage'
+
+
+class MaintenanceEvent(models.Model):
+    id = models.CharField(max_length=255, primary_key=True)
+    type = models.CharField(max_length=50)
+    date = models.DateTimeField()
+    hours_at_service = models.FloatField(db_column='hoursAtService')
+    notes = models.TextField(null=True)
+    machine_id = models.CharField(max_length=255, db_column='machineId')
+    tenant_id = models.CharField(max_length=255, db_column='tenantId')
+
+    class Meta:
+        managed = False
+        db_table = 'MaintenanceEvent'
